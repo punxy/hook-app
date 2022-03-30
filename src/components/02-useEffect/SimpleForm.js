1e8dad7f8843b3db47bf995715fb2af3343e86f5
Sebastián Silva <sebasilvac88@gmail.com>
2022-03-29 23:25:40 -0300

    useEffect

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const {name, email} = formState;
+
+    useEffect( () => {
+        console.log('Hey!');
+    }, []);
+
+    useEffect( () => {
+        console.log('el formulario ha cambiado!');
+    }, [formState]);
+
+    useEffect( () => {
+        console.log('el email ha cambiado!');
+    }, [email]);
+
+    const handleInputChange = ({target}) => {  
+        setFormState({
+            ...formState,
+            [ target.name ]: target.value
+        });
+    }
+
+  return (
+    <>
+        <h1>useEffect</h1>
+        <hr />
+
+        <div className='form-group'>
+            <input
+                type="text"
+                name='name'
+                className='form-control'
+                placeholder='Your name'
+                autoComplete='off'
+                value={name}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <div className='form-group'>
+            <input
+                type="text"
+                name='email'
+                className='form-control'
+                placeholder='your@email.com'
+                autoComplete='off'
+                value={email}
+                onChange={handleInputChange}
+            />
+        </div>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import { HookApp } from './HookApp';
-import { CounterApp } from './components/01-useState/CounterApp';
+//import { CounterApp } from './components/01-useState/CounterApp';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-    <CounterApp />,
+    <SimpleForm />,
   document.getElementById('root')
 );
 
